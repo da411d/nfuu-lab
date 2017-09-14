af3afd1c1f62c66ed9e3bb26b7766c0b84b61d31
Manzhula David <da411d@gmail.com>
2017-09-14 15:59:43 +0300

    Теорія Ймовірностей. Лаба 1.

---
+++ TI/1.R
@@ -0,0 +1,21 @@
+#Чистим екран
+cat("\014")
+
+#Завантажити дані з файла hospital-data.csv в таблицю даних data
+data <- read.csv(file="D:/_PROJECTS/_LAB/TI/_DATA/Лабораторні/hospital-data.csv", header=TRUE, sep=",")
+
+#Перші 3 записи
+head(data, n=3)
+#Останні 3 записи
+tail(data, n=3)
+
+#Загальна інфа
+summary(data)
+
+#Графік
+plot(data[13])
+
+#Фільтрування
+data[data$State == 'AZ', ]
+data[data$Emergency.Services == 'No', ]
+
