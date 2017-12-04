1818ed9d7c428985abf4f667a69ad5cc1b3438cd
Manzhula David <da411d@gmail.com>
2017-12-04 19:22:51 +0200

    :heavy_check_mark: OS-03
    Модульний тиждень розпочався. Пора й практичні робити)

--- OS/LAB3/LAB3/Main.cpp
+++ OS/LAB3/LAB3/Main.cpp
@@ -1,40 +1,78 @@
 #include <Windows.h>
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <math.h>
 #include <conio.h>
 using namespace std;
 CRITICAL_SECTION cs;
+
+float x = 2.7 * 5;
+const int n = 10, m = 5;
+float a[n][m], b[n][m];
+float _a(float i, float j);
+float _b(float i, float j);
+
 DWORD WINAPI thread(LPVOID iNum){
-	float y;
 	int i, j;
+	/*
+		INIT && SORT ARRAY
+	*/
+	for(j=0; j<m; j++){
+		for(i=0; i<n; i++){
+			b[i][j] = _b(i, j);
+			Sleep(7);
+		}
+	} 
+	sort(*b, *b + m*n);
+
+	/*
+		OUTPT
+	*/
 	EnterCriticalSection(&cs); 
-	for(j=0; j<10; j++){
-		for(i=0; i<10; i++){
-			cout << "i"<<j << " " << flush;
+	cout << "/-  B  -/ \r\n";
+	for(i=0; i<n; i++){
+		for(j=0; j<m; j++){
+			cout << b[i][j] << "  " << flush;
 			Sleep(7);
 		}
 		cout << endl;
-	}
+	} 
+	cout << endl;
 	LeaveCriticalSection(&cs); 
 	return 0;
 }
 
 int main(){
+	/*
+		MAGIC
+	*/
 	int i, j;
-	float x;
 	HANDLE hThread;
 	DWORD IDThread;
 	InitializeCriticalSection(&cs);
 	hThread = CreateThread(NULL, 0, thread, (void*)1, 0, &IDThread);
-	if(hThread == NULL){
-		return GetLastError();
-	}
+	if(hThread == NULL)return GetLastError();
 	
-	
+	/*
+		INIT && SORT ARRAY
+	*/
+	for(j=0; j<m; j++){
+		for(i=0; i<n; i++){
+			a[i][j] = _a(i, j);
+			Sleep(7);
+		}
+	} 
+	sort(*a, *a + m*n);
+
+	/*
+		OUTPT
+	*/
 	EnterCriticalSection(&cs); 
-	for(j=0; j<10; j++){
-		for(i=0; i<10; i++){
-			cout << "j"<<j << " " << flush;
+	cout << "/-  A  -/ \r\n";
+	for(i=0; i<n; i++){
+		for(j=0; j<m; j++){
+			cout << a[i][j] << "  " << flush;
 			Sleep(7);
 		}
 		cout << endl;
@@ -42,4 +80,16 @@ int main(){
 	LeaveCriticalSection(&cs);
 	getch();
 	WaitForSingleObject(hThread, INFINITE);
+	return 0;
+}
+
+
+
+float _a(float i, float j){
+	float t = (i-j) / (i + j*j/2);
+	return floor(t*100 + 0.5)/100;
+}
+float _b(float i, float j){
+	float t = (i*i - cos(j)) / (x + 1.5 + i*j);
+	return floor(t*100 + 0.5)/100;
 }
