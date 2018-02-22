8d29bf3151614a52751d6880df892b0a31176f93
Manzhula David <da411d@gmail.com>
2018-02-22 14:29:04 +0200

    :heavy_check_mark: JAVA-07

---
+++ JAVA/LAB_7.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LAB_7{
+	public static void main(String[] args) throws Exception{
+		System.out.println("	 -= LAB 7 =-	 ");
+		System.out.println("  by David Manzhula  ");
+		System.out.println("");
+		System.out.println("");
+		
+		String n1 = "A";
+		System.out.println((int)n1.charAt(0));
+
+		int n2 = 17;
+		System.out.println(n2 + "(10) = " + Integer.toOctalString(n2) + "(8)");
+	}
+}
