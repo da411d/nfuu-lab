82c9f5866585c52ff158a7e9d9b8d3787c2573af
Manzhula David <da411d@gmail.com>
2018-05-09 23:31:20 +0300

    bigInt

--- OOP/Сoursework/src/src/bigInt.h
+++ OOP/Сoursework/src/src/bigInt.h
@@ -1,3 +1,4 @@
+/*
 #pragma once
 #include <string>
 #include "charArray.h"
@@ -5,11 +6,51 @@ using namespace std;
 
 class bigInt : public charArray
 {
-private:
+protected:
+	int* value;
+
 public:
+	bigInt(char* input){
+		int* v = new int[strlen(input)];
+		for(int i = 0; i < strlen(input); i++){
+			v[i] = input[i] - 'a';
+		}
+
+		this->value = v;
+		this->size = strlen(input);
+	};
+	bigInt(bigInt* input){
+		int* v = new int[input->getSize()];
+		for(int i = 0; i < input->getSize(); i++){
+			v[i] = input->value[i];
+		}
+
+		this->value = v;
+		this->size = input->getSize();
+	};
+
+
 	//Operators
-	bigInt operator+(bigInt val){
-		return *this;
+	bigInt operator+(bigInt that){
+		bigInt a = new bigInt(*this);
+		bigInt b = new bigInt(that);
+		int length;
+		if(a.getSize() > b.getSize()){
+			length = a.getSize() + 1;
+		}
+		else{
+			length = b.getSize() + 1;
+		}
+		for(int i = 0; i < length; i++){
+			b.value[i] += a.value[i];
+			b.value[i + 1] += (b.value[i] / 10);
+			b.value[i] %= 10;
+		}
+
+		if(b.value[length - 1] == 0)
+			length--;
+
+		return b;
 	};
 	bigInt operator-(bigInt val){
 		return *this;
@@ -23,10 +64,10 @@ public:
 
 
 	bool operator<(bigInt right){
-		if(strlen(this->value) < strlen(right.value)){
+		if(this->size < right.size){
 			return true;
 		}
-		else if(strlen(this->value) > strlen(right.value)){
+		else if(this->size > right.size){
 			return false;
 		}
 		else{ //If same length
@@ -42,7 +83,7 @@ public:
 		}
 	};
 	bool operator==(bigInt right){
-		if(strlen(this->value) != strlen(right.value))return false;
+		if(this->size != right.size)return false;
 		for(int i = 0; this->value[i]; i++){
 			if(this->value[i] != right.value[i]){
 				return false;
@@ -54,9 +95,25 @@ public:
 		return !(*this < right || *this == right);
 	};
 
-	void operator=(char* val){
-		strcpy(value, val);
+	void operator=(char* input){
+		int* v = new int[strlen(input)];
+		for(int i = 0; i < strlen(input); i++){
+			v[i] = input[i] - 'a';
+		}
+
+		this->value = v;
+		this->size = strlen(input);
 	};
+	void operator=(bigInt* input){
+		int* v = new int[input->getSize()];
+		for(int i = 0; i < input->getSize(); i++){
+			v[i] = input->value[i];
+		}
+
+		this->value = v;
+		this->size = input->getSize();
+	};
+
 	friend ostream& operator<<(ostream &io, bigInt val){
 		io << val;
 		return io;
@@ -65,4 +122,4 @@ public:
 		io >> val;
 		return io;
 	};
-};
+};*/
